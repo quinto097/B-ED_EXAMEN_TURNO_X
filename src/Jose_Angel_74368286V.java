e9e3d3217023fe4b50db2eaf17ba05e0fca4bd48
ExamenJoseAngel <ExamenJoseAngel@gmail.com>
2022-01-17 16:32:46 +0100

    versión final con javadoc DNI 74368286V

--- src/Jose_Angel_74368286V.java
+++ src/Jose_Angel_74368286V.java
@@ -1,19 +1,34 @@
 
 import java.util.*;
 
-
+/**
+ *
+ * @author Jose Ángel Quinto Ferrández / DNI 74368286V
+ * @version 1.0
+ */
 public class Jose_Angel_74368286V {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
-
+/**
+ * 
+ * @param nombreCompleto Nombre completo en mayúsculas.
+ * @param dni Documento nacional de identidad con letra y sin espacios.
+ * 
+ */
     public Jose_Angel_74368286V(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
 
-    
+    /**
+     * 
+     * @param horasExtra  Número de horas extra dedicadas redondeado a la baja (entero)
+     * @metod getSalarioEsteMes Calcula el salario bruto mensual en función del salario base por hora y
+        las horas extras
+     * @return Devuelve el salario bruto mensual en euros con dos decimales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora
@@ -23,7 +38,7 @@ public class Jose_Angel_74368286V {
         return salarioFinal;
     }
 
-    
+    // Este método no lo comentaremos con JavaDoc.
     public static void main(String[] args) {
         Jose_Angel_74368286V emp = new Jose_Angel_74368286V("Jose Angel Quinto",
                 "74368286V");
